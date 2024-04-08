67ecfa665ab75c77c7fe7b8f8587a523b59a643a
ahmedragaae0120 <ahmedmohamed.ar30@gmail.com>
2024-04-08 03:49:03 +0200

    add routes

--- lib/core/Utils/routes.dart
+++ lib/core/Utils/routes.dart
@@ -1,4 +1,6 @@
 class Routes {
   static const String homeRouteName = "homeScreen";
   static const String splashRouteName = "splashScreen";
+  static const String loginRouteName = "loginScreen";
+  static const String registerRouteName = "registerScreen";
 }
