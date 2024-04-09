17fbe50b1e40819e5425f20b2857c69408bc1974
ahmedragaae0120 <ahmedmohamed.ar30@gmail.com>
2024-04-09 18:17:36 +0200

    Connect your app to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movies_app/MyApp.dart';
+import 'package:movies_app/firebase_options.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(
     const MyApp(),
   );
